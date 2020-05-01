eda2383dcc1b3a13ab93abf925442c13adbd1e15
FredericoMartins0 <frederico.martins@edu.pucrs.br>
2020-05-01 15:00:55 -0300

    Adicionado projeto Unity

---
+++ MenusRV/Assets/GoogleVR/Scripts/PlayerMove.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public int playerSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Camera.main.transform.forward * playerSpeed * Time.deltaTime;
+    }
+}
