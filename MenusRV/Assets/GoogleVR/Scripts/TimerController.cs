22b180ab7fe137561c1e7d6471bb0df8b234cf87
FredericoMartins0 <frederico.martins@edu.pucrs.br>
2020-05-11 19:48:25 -0300

    Adicionado timer

---
+++ MenusRV/Assets/GoogleVR/Scripts/TimerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerController : MonoBehaviour
+{
+    public bool press = false;
+
+    static float timer = 0.0f;
+    public Text text_box;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        var min = Mathf.Floor(timer / 60);
+        var sec = timer % 60;
+        text_box.text = string.Format("{0:00} : {1:00}", min, sec);
+    }
+}
